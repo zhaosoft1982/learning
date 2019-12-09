84f29139c2ab4af1da518c8141b68567d3b6247a
xiaoleizhao <xiaoleizhao@creditease.cn>
2019-12-09 09:57:30 +0800

    初始化新Example

--- javaagent01/src/main/java/cn/iocoder/learning/javaagent01/Agent.java
+++ javaagent01/src/main/java/cn/iocoder/learning/javaagent01/Agent.java
@@ -4,14 +4,14 @@ import java.lang.instrument.Instrumentation;
 
 public class Agent {
 
-//    public static void premain(String agentArgs) {
-//        System.out.println("我是一个萌萌哒的 Java Agent");
-//        try {
-//            Thread.sleep(2000L);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public static void premain(String agentArgs) {
+        System.out.println("我是一个萌萌哒的 Java Agent");
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void premain(String agentArgs, Instrumentation instrumentation)  {
 
