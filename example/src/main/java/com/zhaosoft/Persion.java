e6a2bdba2cb714186f698446540ed5f91c79884a
xiaoleizhao <xiaoleizhao@creditease.cn>
2019-12-11 09:56:15 +0800

    初始化新Example

---
+++ example/src/main/java/com/zhaosoft/Persion.java
@@ -0,0 +1,17 @@
+package com.zhaosoft;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author xiaoleizhao
+ * @create 2019-12-10 11:14
+ **/
+@Getter
+@Setter
+public class Persion {
+    private Integer id;
+    private String name;
+    private Integer age;
+}
+
---
+++ example/src/main/java/com/zhaosoft/Test.java
@@ -0,0 +1,16 @@
+package com.zhaosoft;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author xiaoleizhao
+ * @create 2019-12-10 11:15
+ **/
+@Slf4j
+public class Test {
+    public static void main(String [] args ){
+        Persion p=new Persion();
+
+    }
+}
+
