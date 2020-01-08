0fad60988a42553cc207b719adac4dcd6b5d10dd
zhaosoft <zhaosoft1982@163.com>
2020-01-08 23:26:42 +0800

    agent代码初始化

---
+++ algorithmic/src/main/java/com/zhaosoft/example/exam146/Node.java
@@ -0,0 +1,12 @@
+package com.zhaosoft.example.exam146;
+
+public class Node {
+    public int key, val;
+    public Node next, prev;
+
+    public Node(int k, int v) {
+        this.key = k;
+        this.val = v;
+    }
+
+}
