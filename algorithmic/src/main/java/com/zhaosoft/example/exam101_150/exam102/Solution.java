af4411b2f33b9e44c61247ebf7ac58af6f32a44d
xiaoleizhao <xiaoleizhao@creditease.cn>
2023-05-30 21:24:05 +0800

    柠檬水找零

--- algorithmic/src/main/java/com/zhaosoft/example/exam101_150/exam102/Solution.java
+++ algorithmic/src/main/java/com/zhaosoft/example/exam101_150/exam102/Solution.java
@@ -5,6 +5,8 @@ import com.zhaosoft.example.TreeNode;
 import java.util.*;
 
 /**
+ * 102. 二叉树的层序遍历
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
  * 广度优先搜索，遍历树节点
  */
 public class Solution {
@@ -13,25 +15,21 @@ public class Solution {
         if (root == null) {
             return result;
         }
-
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> currentLevel = new ArrayList<>();
 
-        //Set visited=new HashSet();
-
-        while (!q.isEmpty()){
-            int levelSize=q.size();
-            List<Integer> currentLevel=new ArrayList<>();
-
-            for(int i=0;i<levelSize;i++){
-                TreeNode currNode=q.poll();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = q.poll();
                 currentLevel.add(currNode.val);
 
-                if(currNode.left!=null){
+                if (currNode.left != null) {
                     q.add(currNode.left);
                 }
 
-                if(currNode.right!=null){
+                if (currNode.right != null) {
                     q.add(currNode.right);
                 }
             }
--- algorithmic/src/main/java/com/zhaosoft/example/exam101_150/exam102/Solution_BFS.java
+++ algorithmic/src/main/java/com/zhaosoft/example/exam101_150/exam102/Solution_BFS.java
@@ -6,6 +6,11 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 102. 二叉树的层序遍历
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * 广度优先搜索，遍历树节点
+ */
 public class Solution_BFS {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> resultList = new ArrayList<>();
@@ -21,6 +26,7 @@ public class Solution_BFS {
             for (int i = 0; i < levelSize; i++) {
                 TreeNode node = queue.pop();
                 currentLevel.add(node.val);
+                //下层放入队列，从左到右遍历，先放左子节点
                 if (node.left != null) {
                     queue.add(node.left);
                 }
