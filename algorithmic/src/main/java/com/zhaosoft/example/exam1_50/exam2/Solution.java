d017af43a19aa3e8c21dbc4c8511c0921003719d
xiaoleizhao <xiaoleizhao@creditease.cn>
2023-06-07 21:42:46 +0800

    柠檬水找零

--- algorithmic/src/main/java/com/zhaosoft/example/exam1_50/exam2/Solution.java
+++ algorithmic/src/main/java/com/zhaosoft/example/exam1_50/exam2/Solution.java
@@ -2,10 +2,20 @@ package com.zhaosoft.example.exam1_50.exam2;
 
 import com.zhaosoft.example.ListNode;
 
+/**
+ * 2. 两数相加
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //定义存储结果的链表
         ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode curr = dummyHead;
+        //进位
         int carry = 0;
 
         while (p != null || q != null) {
@@ -19,6 +29,7 @@ public class Solution {
             if (q != null) q = q.next;
 
         }
+        //如果还有进位，进位到最高位
         if (carry > 0) {
             curr.next = new ListNode(carry);
         }
