12b4075542157a76ff5ccc873977b7dea25854dc
xiaoleizhao <xiaoleizhao@creditease.cn>
2023-06-12 21:13:48 +0800

    两两交换链表的节点

--- algorithmic/src/main/java/com/zhaosoft/example/exam1_50/exam26/Solution.java
+++ algorithmic/src/main/java/com/zhaosoft/example/exam1_50/exam26/Solution.java
@@ -1,5 +1,14 @@
 package com.zhaosoft.example.exam1_50.exam26;
 
+/**
+ * 26. 删除有序数组中的重复项
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ * 元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+ * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+ * 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。
+ * nums 的其余元素与 nums 的大小不重要。
+ * 返回 k 。
+ */
 public class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
@@ -12,7 +21,7 @@ public class Solution {
                 nums[p1] = nums[p2];
             }
         }
-        return p1+1;
+        return p1 + 1;
     }
 
 
